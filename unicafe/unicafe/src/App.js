55769dbb5040275331bb1118ebceea2011e67f48
Abi0l9 <cryptog3ms@gmail.com>
2022-12-08 12:18:19 +0100

    1.7: unicafe step2

--- unicafe/unicafe/src/App.js
+++ unicafe/unicafe/src/App.js
@@ -7,9 +7,16 @@ function App() {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
   const [allClicks, setAllClicks] = useState([]);
+  let average = [good, bad].reduce((a, b) => a + b, 0) / 3;
 
   const handleClick = (fn, state) => {
     fn(state + 1);
+
+    setAllClicks(allClicks.concat("state"));
+  };
+
+  const handleBadClick = (fn, state) => {
+    fn(state - 1);
     setAllClicks(allClicks.concat("state"));
   };
 
@@ -21,15 +28,17 @@ function App() {
         <button onClick={() => handleClick(setNeutral, neutral)}>
           neutral
         </button>
-        <button onClick={() => handleClick(setBad, bad)}>bad</button>
+        <button onClick={() => handleBadClick(setBad, bad)}>bad</button>
       </div>
       <br />
       <div>
         <h2>statistics</h2>
         <p>good {good}</p>
         <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
+        <p>bad {Math.abs(bad)}</p>
         <p>all {allClicks.length}</p>
+        <p>average {average}</p>
+        <p>positive {good ? (good / allClicks.length) * 100 : 0}</p>
       </div>
     </div>
   );
