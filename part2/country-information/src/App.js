264ef08c1fb1df7727e19ce74180878f9137389b
Abi0l9 <cryptog3ms@gmail.com>
2022-12-15 12:07:42 +0100

    event handlers defined

--- part2/country-information/src/App.js
+++ part2/country-information/src/App.js
@@ -1,7 +1,48 @@
 import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
-  return <div>Init</div>;
+  const [countries, setCountries] = useState([]);
+  const [input, setInput] = useState("");
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    console.log("event started");
+    axios
+      .get("https://restcountries.com/v3.1/all")
+      .then((response) => setCountries(response.data));
+    console.log("Completed");
+  }, []);
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleForm = () => {
+    const searchResults = countries
+      .filter((country) =>
+        country.name.common.toLowerCase().includes(input.toLowerCase())
+      )
+      .map((country) => country.name.common);
+
+    setResults(searchResults);
+  };
+
+  console.log(results);
+  return (
+    <div>
+      <form onChange={handleForm}>
+        <label>find countries </label>
+        <input type="text" value={input} onChange={handleChange} />
+      </form>
+      <div>
+        {results.length
+          ? results.map((result) => <div key={result}>{result}</div>)
+          : "Nothing to display!"}
+      </div>
+    </div>
+  );
 }
 
 export default App;
