5b0091d731b3d78ec667ec6452b1942f0475c726
Abi0l9 <cryptog3ms@gmail.com>
2022-12-27 00:13:46 +0100

    phonebook backend step1

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,31 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
-const PORT = 8000;
+const PORT = 3001;
+
+const persons = [
+  {
+    id: 1,
+    name: "Khalifah",
+    number: "09065909090",
+  },
+  {
+    id: 2,
+    name: "Kashy",
+    number: "08090765432",
+  },
+  {
+    id: 3,
+    name: "Wale",
+    number: "09058786758",
+  },
+  {
+    id: 4,
+    name: "Abiola",
+    number: "08078685848",
+  },
+];
+
+app.get("/api/persons", (req, res) => res.json(persons));
 
 app.listen(PORT, console.log("\n App running on port ", PORT));
