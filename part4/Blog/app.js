b731293657f0c528ac4563150d4ef1b99a51c69d
Abi0l9 <cryptog3ms@gmail.com>
2023-01-13 09:31:59 +0100

    remove gitignore files

--- part4/Blog/app.js
+++ part4/Blog/app.js
@@ -1,26 +1,26 @@
 const config = require("./utils/config");
-const requestLogger = require("./utils/middleware");
+// const requestLogger = require("./utils/middleware");
 const http = require("http");
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const blogRouter = require("./controller/blog");
 const mongoose = require("mongoose");
+// const blogSchema = mongoose.Schema({
+//   title: String,
+//   author: String,
+//   url: String,
+//   likes: Number,
+// });
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-blogSchema.set("toJSON", {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  },
-});
-const Blog = mongoose.model("Blog", blogSchema);
+// blogSchema.set("toJSON", {
+//   transform: (document, returnedObject) => {
+//     returnedObject.id = returnedObject._id.toString();
+//     delete returnedObject._id;
+//     delete returnedObject.__v;
+//   },
+// });
+// const Blog = mongoose.model("Blog", blogSchema);
 
 mongoose.set("strictQuery", true);
 mongoose.connect(config.MONGODB_URI).then(() => {
@@ -31,22 +31,24 @@ app.use(cors());
 app.use(express.json());
 app.use(requestLogger);
 
-app.get("/info", (req, res) => res.send("Welcome!"));
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
+app.use("/api/blogs", blogRouter);
+// app.get("/info", (req, res) => res.send("Welcome!"));
+// app.get("/api/blogs", (request, response) => {
+//   Blog.find({}).then((blogs) => {
+//     response.json(blogs);
+//   });
+// });
 
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
+// app.post("/api/blogs", (request, response) => {
+//   const blog = new Blog(request.body);
 
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
+//   blog.save().then((result) => {
+//     response.status(201).json(result);
+//   });
+// });
 
-const PORT = 3003;
+const PORT = config.PORT;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
