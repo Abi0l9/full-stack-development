98f97db7995390bf876aae35aab6f2743a789273
Abi0l9 <cryptog3ms@gmail.com>
2023-01-14 00:50:36 +0100

    4.4: helper functions and unit tests, step2

--- part4/Blog/utils/list_helper.js
+++ part4/Blog/utils/list_helper.js
@@ -1,3 +1,11 @@
 const dummy = (blogs) => 1;
 
-module.exports = { dummy };
+const totalLikes = (blogs) => {
+  const likes = blogs
+    .map((blog) => blog.likes)
+    .reduce((sum, item) => sum + item, 0);
+
+  return blogs.length > 1 ? likes : blogs[0].likes;
+};
+
+module.exports = { dummy, totalLikes };
