616d2a5570c0f21ef4bc6fdc619b7f5b9b9eac4d
Abi0l9 <cryptog3ms@gmail.com>
2023-02-04 00:15:56 +0100

    5.7 and 5.8 done

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,53 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useRef, useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [display, setDisplay] = useState(false);
+
+  const toggleBlog = { display: display ? "" : "none" };
+  const titleColor = { backgroundColor: display ? "yellow" : "" };
+  const buttonRef = useRef(null);
+
+  const style = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const toggleView = () => {
+    if (display) {
+      buttonRef.current.textContent = "view";
+    } else {
+      buttonRef.current.textContent = "hide";
+    }
+    setDisplay(!display);
+  };
+
+  return (
+    <div style={style}>
+      <div>
+        <p>
+          <span style={titleColor} onClick={toggleView}>
+            {blog.title}
+          </span>
+          {"  "}
+          <button ref={buttonRef} onClick={toggleView}>
+            view
+          </button>
+        </p>
+      </div>
+      <div style={toggleBlog}>
+        <p>{blog.url}</p>
+        <div>
+          <p>
+            likes {blog.likes} <button>like</button>
+          </p>
+        </div>
+        <p>{blog.author}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
