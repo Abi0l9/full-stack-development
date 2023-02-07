b72b14558a3a96e6bdf3f3cfb53b306c85600c6b
Abi0l9 <cryptog3ms@gmail.com>
2023-02-07 22:10:09 +0100

    5.13 blog list tests, step1

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -6,13 +6,25 @@ import Blog from "./Blog";
 
 describe("<Blog />", () => {
   let container;
+  const user = { username: "Kash" };
+  const blog = {
+    title: "test blog",
+    author: "kash",
+    likes: "4",
+    url: "www.com",
+    user: { username: "kash" },
+  };
 
   beforeEach(() => {
-    container = render(<Blog />).container;
+    container = render(<Blog user={user} blog={blog} />).container;
   });
 
   test("container test", () => {
     const div = container.querySelector(".visibleArea");
     screen.debug(div);
+    expect(div).toHaveTextContent("Title");
+    expect(div).not.toHaveTextContent("Url");
+    expect(div).not.toHaveTextContent("Likes");
+    expect(div).not.toHaveTextContent("Author");
   });
 });
