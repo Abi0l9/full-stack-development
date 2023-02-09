2fac5736cbad5abc0a4a28c27d9bbb6278bf8b8b
Abi0l9 <cryptog3ms@gmail.com>
2023-02-09 22:45:49 +0100

    cypress added to frontend

--- part5/bloglist-frontend/src/components/NewBlog.test.js
+++ part5/bloglist-frontend/src/components/NewBlog.test.js
@@ -2,7 +2,6 @@ import React from "react";
 import "@testing-library/jest-dom/extend-expect";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import Blog from "./NewBlog";
 import NewBlog from "./NewBlog";
 
 describe("<NewBlog />", () => {
