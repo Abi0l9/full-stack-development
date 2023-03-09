47d86859bd6341483b06dff3d9016d0c5a722ad2
Abi0l9 <cryptog3ms@gmail.com>
2023-03-09 11:50:50 +0100

    task 6.22

--- part6/query-anecdotes/src/App.js
+++ part6/query-anecdotes/src/App.js
@@ -5,11 +5,23 @@ import Notification from "./components/Notification";
 
 const App = () => {
   const queryClient = useQueryClient();
+
   const updateMutation = useMutation(updateAnecdote, {
-    onSuccess: () => {
-      queryClient.invalidateQueries("anecdotes");
+    onSuccess: (updatedAnecdote) => {
+      const anecdotes = queryClient.getQueryData("anecdotes");
+
+      //update the data with the new result
+      const allAnecs = anecdotes.map((anec) => {
+        if (anec.id === updatedAnecdote.id) {
+          return { ...anec, votes: updatedAnecdote.votes };
+        }
+        return anec;
+      });
+
+      queryClient.setQueryData("anecdotes", allAnecs);
     },
   });
+
   const handleVote = (anecdote) => {
     let votes = anecdote.votes + 1;
     let newObj = { id: anecdote.id, votes };
@@ -22,7 +34,7 @@ const App = () => {
     return <div>Anecdote service is not available due to server problems</div>;
   }
 
-  const anecdotes = result.data;
+  const anecdotes = result.data.sort((a, b) => b.votes - a.votes);
 
   return (
     <div>
