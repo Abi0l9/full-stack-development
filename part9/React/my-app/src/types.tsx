65400a4a31e247c344889ba483774d354080c767
Abi0l9 <cryptog3ms@gmail.com>
2023-03-18 07:56:42 +0100

    first task of 9.15 done

--- part9/React/my-app/src/types.tsx
+++ part9/React/my-app/src/types.tsx
@@ -2,3 +2,31 @@ export interface ContentType {
   name: string;
   exerciseCount: number;
 }
+
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+}
+
+interface CoursePartDescription extends CoursePartBase {
+  description: string;
+}
+
+interface CoursePartBasic extends CoursePartDescription {
+  kind: "basic";
+}
+
+interface CoursePartGroup extends CoursePartBase {
+  groupProjectCount: number;
+  kind: "group";
+}
+
+interface CoursePartBackround extends CoursePartDescription {
+  backroundMaterial: string;
+  kind: "background";
+}
+
+export type CoursePart =
+  | CoursePartBasic
+  | CoursePartGroup
+  | CoursePartBackround;
