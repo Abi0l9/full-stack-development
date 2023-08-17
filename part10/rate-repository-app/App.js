b6e48d16f17f022509fd77fe137887a151c31c1f
Abi0l9 <cryptog3ms@gmail.com>
2023-08-17 09:18:41 +0100

    Exercise 10.3: the reviewed repositories list

--- part10/rate-repository-app/App.js
+++ part10/rate-repository-app/App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+// import { StatusBar } from "expo-status-bar";
+// import { StyleSheet, Text, View } from "react-native";
+import { View } from "react-native";
+import Main from "./src/components/Main";
+import RepositoryList from "./src/components/RepositoryList";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>App works!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Main />
+      <RepositoryList />
     </View>
+    // <View style={styles.container}>
+    //   <Text>App works very fine!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
