208cde4d2db6f1332def96bdeab67441a1de8edf
Abi0l9 <cryptog3ms@gmail.com>
2023-08-18 10:08:11 +0100

    Exercise 10.7: scrollable app bar

--- part10/rate-repository-app/src/components/AppBar.jsx
+++ part10/rate-repository-app/src/components/AppBar.jsx
@@ -1,21 +1,21 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, ScrollView } from "react-native";
 import AppBarTab from "./AppBarTab";
 
 const styles = StyleSheet.create({
   container: {
     display: "flex",
-    flexDirection: "row",
     backgroundColor: "#24292e",
   },
-
   // ...
 });
 
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <AppBarTab tabName={"Repositories"} link={"/"} />
-      <AppBarTab tabName={"Sign in"} link={"sign-in"} />
+      <ScrollView horizontal>
+        <AppBarTab tabName={"Repositories"} link={"/"} />
+        <AppBarTab tabName={"Sign in"} link={"sign-in"} />
+      </ScrollView>
     </View>
   );
 };
