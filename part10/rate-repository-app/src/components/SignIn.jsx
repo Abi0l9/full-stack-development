41578338f8309e209d7e48ca5bc2eb64d57a80a3
Abi0l9 <cryptog3ms@gmail.com>
2023-08-18 13:10:20 +0100

    form not rendering

--- part10/rate-repository-app/src/components/SignIn.jsx
+++ part10/rate-repository-app/src/components/SignIn.jsx
@@ -1,7 +1,18 @@
-import { Text } from "react-native";
+// import { Text, View } from "react-native";
+import { Formik } from "formik";
+import FormikTextInput from "./FormikTextInput";
+
+const initialValues = { username: "", password: "" };
 
 const SignIn = () => {
-  return <Text>The sign-in view</Text>;
+  const onSubmit = () => {
+    console.log("submitted");
+  };
+  return (
+    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+      {({ handleSubmit }) => <FormikTextInput name="username" />}
+    </Formik>
+  );
 };
 
 export default SignIn;
